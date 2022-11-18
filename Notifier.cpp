b49941f1081b20f4722b24e581526eb82ba4c20b
FilippoSciammacca <filipposciammacca@libero.it>
2022-11-18 16:44:49 +0100

    Notifier mostra maggiori info quando arriva un nuovo Message

--- Notifier.cpp
+++ Notifier.cpp
@@ -10,8 +10,14 @@ Notifier::Notifier(const std::string &name){
 }
 
 void Notifier::update(Message msg) {
-    using namespace std;
-    cout << "Ehi " << this->name << ", hai ricevuto un messaggio: " << msg.getText() << endl;
+    char time[20];
+    time_t _t = msg.getMyTime();
+    struct tm* localTime = localtime(&_t);
+    strftime(time, 20, "%H:%M:%S", localTime);
+    char date[20];
+    strftime(date, 20, "%d-%m-%Y", localTime);
+
+    std::cout << "Ehi " + this->name + ", hai ricevuto un messaggio!\nAlle " + time + " del " + date + ", " + msg.getSender() + " ha scritto: " + msg.getText() << std::endl;
 }
 
 std::string Notifier::getName() {
