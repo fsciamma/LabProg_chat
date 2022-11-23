d05bdfb66ec1bd758c836e49de4fc27e97997d36
FilippoSciammacca <filipposciammacca@libero.it>
2022-11-23 18:09:12 +0100

    Aggiunto un primo test per i Message

---
+++ test/MessageTest.cpp
@@ -0,0 +1,13 @@
+//
+// Created by filip on 23/11/2022.
+//
+
+#include "gtest/gtest.h"
+#include "../Message.h"
+
+TEST(Message, GetterSetter) {
+    Message msg("Tester", "Questo è un messaggio di prova");
+    ASSERT_EQ(msg.getSender(), "Tester");
+    ASSERT_EQ(msg.getText(), "Questo è un messaggio di prova");
+}
+
---
+++ test/runAllTests.cpp
@@ -0,0 +1,12 @@
+//
+// Created by filip on 23/11/2022.
+//
+
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
