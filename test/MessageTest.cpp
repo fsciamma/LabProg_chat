52946f4aa38609921475be8b0a259df00615556f
FilippoSciammacca <filipposciammacca@libero.it>
2022-11-24 12:21:58 +0100

    Formattazione

--- test/MessageTest.cpp
+++ test/MessageTest.cpp
@@ -2,11 +2,12 @@
 // Created by filip on 23/11/2022.
 //
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 #include "../Message.h"
 
 TEST(Message, GetterSetter) {
     Message msg("Tester", "Questo è un messaggio di prova");
+
     ASSERT_EQ(msg.getSender(), "Tester");
     ASSERT_EQ(msg.getText(), "Questo è un messaggio di prova");
 }
