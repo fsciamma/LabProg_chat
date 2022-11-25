916545b108c8e8b414594871e2f2918a9304e581
FilippoSciammacca <filipposciammacca@libero.it>
2022-11-25 15:58:21 +0100

    UserTest: aggiunto test per i metodi su chat di gruppo

--- test/UserTest.cpp
+++ test/UserTest.cpp
@@ -11,7 +11,34 @@ TEST(User, chatMethods){
 
     ASSERT_NO_THROW(u1->createChat(u2));
     ASSERT_THROW(u2->createChat(u1), std::runtime_error);
+}
 
+TEST(User, groupMethods){
+    User* u1 = new User("Tester");
+    User* u2 = new User("SecondTester");
     User* u3 = new User("ThirdTester");
 
+    std::vector<User*> testers;
+    testers.push_back(u2);
+    testers.push_back(u3);
+    ASSERT_NO_THROW(u1->createGroupChat(testers, "GroupTest"));
+    ASSERT_THROW(u1->createGroupChat(testers, "GroupTest"), std::runtime_error);
+
+    User* u4 = new User("FourthTester");
+
+    std::cout << "u1 e u3 ricevono il messaggio di u2; u4 non fa parte del gruppo." << std::endl;
+    ASSERT_NO_THROW(u2->sendMessage("Messaggio di test", "GroupTest"));
+    ASSERT_THROW(u4->sendMessage("Messaggio di test", "GroupTest"), std::runtime_error);
+    std::cout << std::endl;
+
+    u1->addUserToGroupChat("GroupTest", u4);
+    std::cout << "u2, u3 e u4 ricevono il messaggio di u1." << std::endl;
+    ASSERT_NO_THROW(u1->sendMessage("Messaggio di test", "GroupTest"));
+    u1->kickUserFromGroupChat("GroupTest", u2);
+    u1->leaveGroup("GroupTest");
+    std::cout << std::endl;
+
+    std::cout << "Solo u4 riceve il messaggio di u3; u1 e u2 non fanno più parte del gruppo." << std::endl;
+    ASSERT_NO_THROW(u3->sendMessage("Messaggio di test", "GroupTest"));
+    ASSERT_THROW(u2->sendMessage("Messaggio di test", "GroupTest"), std::runtime_error);
 }
