b02ce1efc5b8632982b74cdefd6b805f52928fb0
FilippoSciammacca <filipposciammacca@libero.it>
2022-11-28 11:25:13 +0100

    UserTest: aggiunti dei test

--- test/UserTest.cpp
+++ test/UserTest.cpp
@@ -11,6 +11,11 @@ TEST(User, chatMethods){
 
     ASSERT_NO_THROW(u1->createChat(u2));
     ASSERT_THROW(u2->createChat(u1), std::runtime_error);
+
+    ASSERT_NO_THROW(u1->sendMessage("Messaggio di test", "OtherTester"));
+    ASSERT_NO_THROW(u2->readChat("Tester"));
+    ASSERT_NO_THROW(u2->readLastMessageFrom("Tester"));
+
 }
 
 TEST(User, groupMethods){
@@ -27,18 +32,20 @@ TEST(User, groupMethods){
     User* u4 = new User("FourthTester");
 
     std::cout << "u1 e u3 ricevono il messaggio di u2; u4 non fa parte del gruppo." << std::endl;
-    ASSERT_NO_THROW(u2->sendMessage("Messaggio di test", "GroupTest"));
-    ASSERT_THROW(u4->sendMessage("Messaggio di test", "GroupTest"), std::runtime_error);
+    ASSERT_NO_THROW(u2->sendMessage("Messaggio di test - 1", "GroupTest"));
+    ASSERT_THROW(u4->sendMessage("Messaggio di test - 2", "GroupTest"), std::runtime_error);
+    ASSERT_THROW(u4->readChat("GroupTest"), std::runtime_error);
     std::cout << std::endl;
 
     u1->addUserToGroupChat("GroupTest", u4);
     std::cout << "u2, u3 e u4 ricevono il messaggio di u1." << std::endl;
-    ASSERT_NO_THROW(u1->sendMessage("Messaggio di test", "GroupTest"));
+    ASSERT_NO_THROW(u1->sendMessage("Messaggio di test - 3", "GroupTest"));
     u1->kickUserFromGroupChat("GroupTest", u2);
     u1->leaveGroup("GroupTest");
     std::cout << std::endl;
 
     std::cout << "Solo u4 riceve il messaggio di u3; u1 e u2 non fanno più parte del gruppo." << std::endl;
-    ASSERT_NO_THROW(u3->sendMessage("Messaggio di test", "GroupTest"));
-    ASSERT_THROW(u2->sendMessage("Messaggio di test", "GroupTest"), std::runtime_error);
+    ASSERT_NO_THROW(u3->sendMessage("Messaggio di test - 4", "GroupTest"));
+    ASSERT_THROW(u2->sendMessage("Messaggio di test - 5", "GroupTest"), std::runtime_error);
+    ASSERT_NO_THROW(u4->readLastMessageFrom("GroupTest"));
 }
