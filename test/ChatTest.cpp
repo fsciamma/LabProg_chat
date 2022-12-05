17bf6a97deb935a6423d1b324206b58d97e09c92
FilippoSciammacca <filipposciammacca@libero.it>
2022-12-05 15:35:45 +0100

    Modificati ChatTest e GroupChatTest

--- test/ChatTest.cpp
+++ test/ChatTest.cpp
@@ -4,11 +4,20 @@
 
 #include <gtest/gtest.h>
 #include "../Chat.h"
+#include "../User.h"
 
 
 TEST(Chat, readMessages){
-    Chat c;
+    User u1("Tester");
+    User u2("SecondTester");
+    u1.createChat(&u2);
+    std::shared_ptr<Chat> c = u1.getMyChats().find("SecondTester")->second;
 
-    ASSERT_THROW(c.readChatMessages(), std::out_of_range);
-    ASSERT_THROW(c.readLastMessage(), std::out_of_range);
+    ASSERT_THROW(c->readChatMessages("SecondTester"), std::out_of_range);
+    ASSERT_THROW(c->readLastMessage("SecondTester"), std::out_of_range);
+
+    u2.sendMessage("Messaggio 1", "Tester");
+
+    ASSERT_NO_THROW(c->readChatMessages("SecondTester"));
+    ASSERT_NO_THROW(c->readLastMessage("SecondTester"));
 }
--- test/GroupChatTest.cpp
+++ test/GroupChatTest.cpp
@@ -4,15 +4,32 @@
 
 #include <gtest/gtest.h>
 #include "../GroupChat.h"
+#include "../User.h"
 
 TEST(GroupChat, Getter){
-    GroupChat* gc = new GroupChat("Group");
-    ASSERT_EQ(gc->getGroupName(), "Group");
+    User u1("Tester");
+    User u2("SecondTester");
+    User u3("ThirdTester");
+    std::vector<User*> vTest;
+    vTest.push_back(&u2);
+    vTest.push_back(&u3);
+    u1.createGroupChat(vTest, "Gruppo");
+    std::shared_ptr<Chat> gc = u1.getMyChats().find("Gruppo")->second;
+
+    ASSERT_EQ(std::dynamic_pointer_cast<GroupChat>(gc)->getGroupName(), "Gruppo");
 }
 
 
 TEST(GroupChat, readMessages){
-    GroupChat* gc = new GroupChat("Group");
-    ASSERT_THROW(gc->readChatMessages(), std::out_of_range);
-    ASSERT_THROW(gc->readLastMessage(), std::out_of_range);
+    User u1("Tester");
+    User u2("SecondTester");
+    User u3("ThirdTester");
+    std::vector<User*> vTest;
+    vTest.push_back(&u2);
+    vTest.push_back(&u3);
+    u1.createGroupChat(vTest, "Gruppo");
+    std::shared_ptr<Chat> gc = u1.getMyChats().find("Gruppo")->second;
+    ASSERT_THROW(std::dynamic_pointer_cast<GroupChat>(gc)->readChatMessages("Tester"), std::out_of_range);
+    ASSERT_THROW(std::dynamic_pointer_cast<GroupChat>(gc)->readLastMessage("SecondTester"), std::out_of_range);
+
 }
